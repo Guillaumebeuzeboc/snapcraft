79e78c6ead0ff32bed1783cfcac6db7f0bef8d47
gbeuzeboc <guillaume.beuzeboc@canonical.com>
2022-05-18 09:27:32 +0200

    WIP

---
+++ tests/spread/plugins/v1/catkin/snaps/ros-node/src/talker/src/ros_node.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <functional>
+
+#include <ros/ros.h>
+
+class Finally
+{
+public:
+  Finally(std::function<void()> cb) : cb_(std::move(cb))
+  {
+  }
+  ~Finally()
+  {
+    cb_();
+  }
+
+private:
+  std::function<void()> cb_;
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle nodeHandle;
+
+  ros::Rate loopRate(10);
+
+  const auto good_bye = Finally([] { std::cout << "Good bye!" << std::endl; });
+  std::cout << "Hello" << std::endl;
+  ros::spin();
+
+  return 0;
+}
